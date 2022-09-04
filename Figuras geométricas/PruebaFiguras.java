abe572e31e4acdd94bacdcef0f7d04efa0a57f45
Elízabeth Alzate <elizas95@gmail.com>
2022-09-03 22:28:40 -0500

    Ejercicios propuestos figuras geométricas

--- Figuras geométricas/PruebaFiguras.java
+++ Figuras geométricas/PruebaFiguras.java
@@ -9,6 +9,8 @@ public class PruebaFiguras {
         Rectangulo miRectangulo = new Rectangulo(1, 2);
         Cuadrado miCuadrado = new Cuadrado(3);
         TrianguloRectangulo miTrianguloRectangulo = new TrianguloRectangulo(3, 5);
+        Rombo miRombo = new Rombo(5,8,4);
+        Trapecio miTrapecio = new Trapecio(15,12,6);
 
         System.out.println("El área del círculo es = " + miCirculo.calcularArea());
         System.out.println("El perímetro del círculo es = " + miCirculo.calcularPerimetro());
@@ -22,6 +24,12 @@ public class PruebaFiguras {
         System.out.println("El área del triángulo es = " + miTrianguloRectangulo.calcularArea());
         System.out.println("El perímetro del triángulo es = " + miTrianguloRectangulo.calcularPerimetro());
         miTrianguloRectangulo.determinarTipoTriangulo();
+        System.out.println();
+        System.out.println("El área del rombo es = " + miRombo.calcularArea());
+        System.out.println("El perímetro del rombo es = " + miRombo.calcularPerimetro());
+        System.out.println();
+        System.out.println("El área de mi trapecio es = " + miTrapecio.calcularArea());
+        System.out.println("El perímetro de mi trapecio es = " + miTrapecio.calcularPerimetro());
     }
 }
 
